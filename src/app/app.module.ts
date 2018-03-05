2305c0955e95316b0cfc36361af9a2831e2ca196
Michael Iskandarani <mike.iskandarani@gmail.com>
2018-03-05 11:22:11 +0200

    First commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
+import { AuthService } from './services/auth/auth.service';
+import { AuthGaurdService } from './services/auth/auth-gaurd.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
+import { AppRoutingModule } from './app.routing';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    DashboardLayoutComponent,
+    StartPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule.forRoot(),
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthGaurdService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,29 @@
+import { StartPageComponent } from './pages/start-page/start-page.component';
+import { AuthGaurdService } from './services/auth/auth-gaurd.service';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+
+export const routes: Routes = [
+
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: '',
+    component: DashboardLayoutComponent,
+    canActivate: [AuthGaurdService],
+    children: [
+      { path: '', component: StartPageComponent, pathMatch: 'full' }
+    ]
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
